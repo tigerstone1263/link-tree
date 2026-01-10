7123782c20a8b0463ebb392fa1606df8b0c40e57
tigerstone1263 <00fanxi@gmail.com>
2026-01-10 11:14:57 -0800

    2

---
+++ app-config.js
@@ -0,0 +1,14 @@
+window.APP_CONFIG = {
+  title: "Absolute Ear",
+  description: "Train Your Absolute & Relative Pitch",
+  logoSrc: "logo512.jpg",
+  logoAlt: "App logo",
+  appStore: {
+    url: "https://apps.apple.com/us/app/absolute-ear-train-your-pitch/id6739764701",
+    label: "App Store"
+  },
+  playStore: {
+    url: "https://play.google.com/store/apps/details?id=com.tigerstone.AbsoluteEar&referrer=utm_source%3Dlink_tree",
+    label: "Play Store"
+  }
+};
